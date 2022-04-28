151a7dbcf18b3f6fcc54536da868362daf088bb2
Kai Yonashiro <kai.yonashiro@gmail.com>
2022-04-28 12:12:41 -1000

    fixing html

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path')
 
-const app = express();
+const app = express()
 
-app.use(cors());
 app.use(express.json())
 
+
+// include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
   accessToken: '1843678992f64c5582e1711d6a22aaf6',
@@ -13,23 +14,37 @@ var rollbar = new Rollbar({
   captureUnhandledRejections: true,
 })
 
-// record a generic message and send it to Rollbar
-rollbar.log('Hello world!')
-
-app.use(express.json()); // When we want to be able to accept JSON.
-
-app.get('/', (req,res) => {
+app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, '../index.html'))
-    rollbar.info('html file served successfully.')
+})
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'../styles.css'))
 })
 
 app.get('/test', () => {try{
-    banana()
+  banana()
 } catch(error){rollbar.error(error)}})
-  
 
-const port = process.env.PORT || 4546
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+const port = process.env.PORT || 4545
 app.use(rollbar.errorHandler())
-app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
-})
+
+app.listen(port, ()=> console.log(`Listening on ${port}`)) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
